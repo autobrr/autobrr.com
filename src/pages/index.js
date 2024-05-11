15f28debddfcda71cbac86c3c2778d1a4f7acb37
s0up4200 <s0up4200@pm.me>
2024-05-11 19:05:53 +0200

    fix: useState

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { useColorMode } from "@docusaurus/theme-common";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./index.module.css";
 import logo from "../../static/img/logo.png";
@@ -32,6 +32,11 @@ const Center = ({ icon, text }) => (
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   const { colorMode } = useColorMode();
+  const [headerImage, setHeaderImage] = useState(FrontPicLight);
+
+  useEffect(() => {
+    setHeaderImage(colorMode === "dark" ? FrontPicDark : FrontPicLight);
+  }, [colorMode]);
 
   return (
     <header
@@ -91,7 +96,7 @@ function HomepageHeader() {
         </div>
       </div>
       <img
-        src={colorMode === "dark" ? FrontPicDark : FrontPicLight}
+        src={headerImage}
         alt="autobrr"
         className={clsx(styles.image, "front-page-pic")}
       />
